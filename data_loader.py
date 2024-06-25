ec75a640723451a20bf5bd65c3ae7a9197b43bcc
AJ Ibraheem <ibraheemaj@outlook.com>
2024-06-25 08:40:57 +0100

    FineWeb EDU training

--- data_loader.py
+++ data_loader.py
@@ -1,5 +1,45 @@
+import os
 import tiktoken
 import torch
+import numpy as np
+
+def load_tokens(filename):
+    npt = np.load(filename)
+    npt = npt.astype(np.int32)
+    ptt = torch.tensor(npt, dtype=torch.long)
+    return ptt
+
+class DataLoaderFineWeb:
+    def __init__(self, B, T, split):
+        self.B = B
+        self.T = T
+        assert split in {'train', 'val'}
+
+        # get the shard filenames
+        data_root = "edu_fineweb10B"
+        shards = os.listdir(data_root)
+        shards = [s for s in shards if split in s]
+        shards = sorted(shards)
+        self.shards = [os.path.join(data_root, s) for s in shards]
+        assert len(self.shards) > 0, f"No Shard found for split {split}"
+        
+        self.current_shard = 0
+        self.tokens = load_tokens(self.shards[self.current_shard])
+        self.current_position = 0
+
+    def next_batch(self):
+        B, T = self.B, self.T
+
+        buf = self.tokens[self.current_position:self.current_position+(B*T)+1]
+        x = (buf[:-1]).view(B, T) #inputs
+        y = (buf[1:]).view(B, T) # targets
+
+        # advance the position in the tensor
+        self.current_position += B * T
+        # if loading the next batch would be out of bounds, reset
+        if self.current_position + (B*T)+1 > len(self.tokens):
+            self.current_position = 0
+        return x, y
 
 class DataLoaderLite:
     def __init__(self, B, T):
@@ -28,7 +68,9 @@ class DataLoaderLite:
 
         # advance the position in the tensor
         self.current_position += B * T
-        # if loading the next batch would be out of bounds, reset
+        # if loading the next batch would be out of bounds, advance to next shard
         if self.current_position + (B*T)+1 > len(self.tokens):
+            self.current_shard = (self.current_shard + 1) % len(self.shards)
+            self.tokens = load_tokens(self.shards[self.current_shard])
             self.current_position = 0
         return x, y
